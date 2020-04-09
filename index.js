5495b43f06ba665af500228c03566d23126e3f6d
Hakan Yildiz <hakan@re-coded.com>
2020-04-09 20:09:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function handleClick(e) {
 function greet(mTime){
 
   let parsedTime = parseInt(mTime.split(':'));
+<<<<<<< HEAD
   if(parsedTime < 12){
     return ('Good Morning');
   }
@@ -21,12 +22,28 @@ function greet(mTime){
   }
   
   else {
+=======
+  if(parsedTime <= 12){
+    return ('Good Morning');
+  }
+  
+  else if( 12<mTime<17){
+    return ('Good Afternoon');
+  }
+  
+  else (){
+>>>>>>> a754a6782ac942757fdaf43e154edceb1b08af5f
     return ('Good Evening');
   }
 }
 /* Write your implementation of displayMessage() */
+<<<<<<< HEAD
 function displayMessage(greet){
    document.getElementById('greeting').innerText = greet ;
   
+=======
+function displayMessage(){
+  document.getElementById('greeting').textContent = greet();
+>>>>>>> a754a6782ac942757fdaf43e154edceb1b08af5f
 }
 
